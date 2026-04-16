acbdb828816d9c9f587f2358a884180ad3420add
Hamzah123456 <alssoudihamzah@gmail.com>
2026-04-16 17:39:09 -0400

    Added queries of actor and film tables

--- DataAnalytics/week-02/Ex1B_first_query.sql
+++ DataAnalytics/week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about cats and dogs for use by pet store.
 */
+USE sakila;
+select * from actor;
+select * from actor; -- Retrieved 200 records
+select * from film; -- retrived 1000 records
