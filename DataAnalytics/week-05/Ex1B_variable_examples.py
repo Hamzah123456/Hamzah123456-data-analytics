1445bf177ff23213229eb3fc648142cb7458858f
Hamzah123456 <alssoudihamzah@gmail.com>
2026-05-04 09:50:14 -0400

    Identification

---
+++ DataAnalytics/week-05/Ex1B_variable_examples.py
@@ -0,0 +1,15 @@
+customer_id = 101
+customer_name = 'John Pork'
+customer_gender = 'Male'
+customer_dob = '01/15/1995'
+drivers_license_number = 'D1234567'
+auto_policy_number = 'AP-98765'
+customer_id = 101 # assumed this would be a number, but some systems use letters too like 'CUST-101'
+customer_name = 'John Pork' # assumed first + last name together, maybe should be separate variables?
+customer_gender = 'Male' # assumed a simple string, but maybe should use M/F/Other for consistency
+customer_dob = '01/15/1995' # assumed MM/DD/YYYY format, but could vary by country or system
+drivers_license_number = 'D1234567' # assumed a string since it can have letters, length varies by state
+auto_policy_number = 'AP-98765' # assumed string with a prefix, format could differ per insurance company
+
+my_name = 'Hamzah'
+birth_city_state = 'New York, NY'
