d1cd4e8dfc3c189d6fdaaa02dcb0c297a9039e26
Hamzah123456 <alssoudihamzah@gmail.com>
2026-05-04 09:40:51 -0400

    making cents

--- DataAnalytics/week-05/Ex1A_hello.py
+++ DataAnalytics/week-05/Ex1A_hello.py
@@ -1,1 +1,19 @@
 print('Hello world!')
+message = 'Hello world!'
+print (message)
+
+# "Hello world" prints twice beacuse its 8AM
+
+
+# Displaying dollars and cents 
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+# The result is 3.5, Python adds the two floats together and displays the sum as a decimal
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + " " + c_str)
